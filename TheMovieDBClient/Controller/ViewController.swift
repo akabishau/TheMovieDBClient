55203a422cc3f5a35094a743974634f3f8efb816
AlekseyK <aleksey.kabishau@gmail.com>
2021-03-10 21:44:33 -0500

    Initial Commit

---
+++ TheMovieDBClient/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDBClient
+//
+//  Created by Aleksey Kabishau on 3/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
