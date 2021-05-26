9a23c260b4459a22b58bdd835fef29069cf0807e
AlekseyK <aleksey.kabishau@gmail.com>
2021-05-25 23:15:15 -0400

    feat: add tableView and display favorite movies

--- TheMovieDBClient/Controller/Core/FavoritesListVC.swift
+++ TheMovieDBClient/Controller/Core/FavoritesListVC.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class FavoritesListVC: UIViewController {
     
+    var tableView: UITableView!
+    
     var favoriteMovies: [Movie] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()        
+        
+        configureTableView()
         
         NetworkManager.shared.getFavoriteMovies { [weak self] result in
             
@@ -29,4 +33,48 @@ class FavoritesListVC: UIViewController {
             }
         }
     }
+    
+    
+    
+    private func configureTableView() {
+        tableView = UITableView(frame: .zero)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
+
+
+extension FavoritesListVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoriteMovies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let movie = favoriteMovies[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = movie.title
+        
+        return cell
+    }
+    
+    
+}
+
+
+extension FavoritesListVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(favoriteMovies[indexPath.row].title)
+    }
+}
+
